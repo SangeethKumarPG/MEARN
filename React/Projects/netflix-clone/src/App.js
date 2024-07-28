4f7edfeaa30ab8f3a3bd3d53207b1a12a51d2d52
sangeethkumarpg <pgsangeethkumar@gmai.com>
2024-07-28 14:39:35 +0530

    netflix clone project adding animations

--- React/Projects/netflix-clone/src/App.js
+++ React/Projects/netflix-clone/src/App.js
@@ -1,22 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Banner from './components/Banner';
 // import Nav from './components/Nav';
 import Row from './components/Row';
 import requests from './request';
+import LoadingPage from './components/LoadingPage';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(false);
+    }, 3000);
+
+  },[])
   return (
     <div>
-      {/* <Nav/> */}
-      <Banner fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending" fetchUrl={requests.fetchTrending}/>
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row fetchUrl={requests.fetchTopRated} title="Top Rated"/>
-      <Row fetchUrl={requests.fetchActionMovies} title="Action Movies"/>
-      <Row fetchUrl={requests.fetchComedyMovies} title="Comedy"/>
-      <Row fetchUrl={requests.fetchDocumentaries} title="Documentary"/>
-      <Row fetchUrl={requests.fetchHorrorMovies} title="Horror"/>
-      <Row fetchUrl={requests.fetchRomanceMovies} title="Romance"/>
+      {isLoading ? (<LoadingPage/> ) 
+      :(
+      <>
+          {/* <Nav/> */}
+        <Banner fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+        <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+        <Row fetchUrl={requests.fetchTopRated} title="Top Rated"/>
+        <Row fetchUrl={requests.fetchActionMovies} title="Action Movies"/>
+        <Row fetchUrl={requests.fetchComedyMovies} title="Comedy"/>
+        <Row fetchUrl={requests.fetchDocumentaries} title="Documentary"/>
+        <Row fetchUrl={requests.fetchHorrorMovies} title="Horror"/>
+        <Row fetchUrl={requests.fetchRomanceMovies} title="Romance"/>
+
+      </>
+      )}
     </div>
   );
 }
--- React/Projects/netflix-clone/src/components/Nav.jsx
+++ React/Projects/netflix-clone/src/components/Nav.jsx
@@ -14,9 +14,15 @@ function Nav() {
             <li>New and Popular</li>
             <li>My List</li>
             <li>Browse by Languages</li>
-
+            <li><i class="fa-solid fa-magnifying-glass"></i></li>
+            <li><i class="fa-regular fa-bell"></i></li>
           </ul>
         </nav>
+       
+          <img src='images/profileicon.jpg' alt="" style={{width:"2rem", height:"2rem", marginTop:"2rem", marginLeft:"1rem"}}/>
+        
+        
+        
     </div>
   )
 }
