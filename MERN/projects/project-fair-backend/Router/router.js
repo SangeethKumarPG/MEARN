36a6c2f45a3bd47c42a1b5aaba97b52c1d059cab
sangeethkumarpg <pgsangeethkumar@gmai.com>
2024-09-10 13:33:43 +0530

    nodejs mvc start, nodejs notes

---
+++ MERN/projects/project-fair-backend/Router/router.js
@@ -0,0 +1,18 @@
+// import express
+const express = require("express");
+
+//get router instance from express
+const router = new express.Router();
+
+// importing controller
+const userController = require('../Controllers/userController')
+
+// differrent paths for resolving requests
+// router.get("/user/getUserDetails", (req, res) => {
+//   res.send("inside getUserDetails route");
+// });
+router.post("/user/register", userController.register);
+router.get("/user/getUserDetails", userController.getUserDetails);
+
+// export router
+module.exports = router;
--- MERN/projects/project-fair-backend/index.js
+++ MERN/projects/project-fair-backend/index.js
@@ -1,1 +1,36 @@
-console.log("hi!!!");
+// importing dotenv
+require("dotenv").config();
+
+//importing express
+const express = require("express");
+
+//importing cors
+const cors = require("cors");
+
+//import router js file which had implemented router
+const router = require("./Router/router");
+
+// create server using express
+const pfServer = express();
+
+//inject cors into pf server
+pfServer.use(cors());
+
+// use middleware to convert JSON data to js object
+pfServer.use(express.json());
+
+//conntect the router
+pfServer.use(router);
+
+// provide port number
+const port = 4000;
+
+// run server
+
+pfServer.listen(port, () => {
+  console.log(`pf server is running on http://localhost:${port}`);
+});
+
+pfServer.get("/", (req, res) => {
+  res.send("Project fair is running and waiting for client request");
+});
