821e6557202824722ce7343840109f599bcc7cd9
sangeethkumarpg <pgsangeethkumar@gmai.com>
2024-10-02 00:38:33 +0530

    To do list app task add, delete, mark as complete (complete)

--- React/Projects/todoapp/src/components/TaskCard.jsx
+++ React/Projects/todoapp/src/components/TaskCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './TaskCard.css'
 import trashIcon from '../assets/trash-icon.svg'
+<<<<<<< HEAD
 
 function TaskCard() {
   const cardClicked = () => {console.log("Card Clicked");}
@@ -12,6 +13,26 @@ function TaskCard() {
             <input type="checkbox" className="form-check-input" id="checkId"/> {" "}
             <label htmlFor="checkId" className="form-check-label">Task Name </label>
         <button className="btn btn-transparent">
+=======
+import {useDispatch} from "react-redux";
+import {markAsComplete, deleteTask} from '../redux/todoSlice'
+
+function TaskCard({taskItem}) {
+  const dispatch = useDispatch();
+  const handleCheckBoxchange = () => {
+    dispatch(markAsComplete(taskItem.id))
+  }
+  const handleDelete = () =>{
+    dispatch(deleteTask(taskItem.id))
+  } 
+  return (
+    <>
+      <div className="container border border-light rounded-2 m-2 p-2 white-shadow d-flex justify-content-evenly align-items-center adjust-width"> 
+            
+            <input type="checkbox" className="form-check-input" id={`checkId-${taskItem.id}`} checked={taskItem.completed} onChange={handleCheckBoxchange} /> {" "}
+            <label htmlFor={`checkId-${taskItem.id}`} className="form-check-label">{taskItem?.completed? <s>{taskItem?.title}</s>:taskItem?.title} </label>
+        <button className="btn btn-transparent" onClick={handleDelete}>
+>>>>>>> 20fe7f9 (To do list app task add, delete, mark as complete (complete))
         <img src={trashIcon} /></button>
         </div>
 
--- React/Projects/todoapp/src/main.jsx
+++ React/Projects/todoapp/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+<<<<<<< HEAD
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+=======
+import {Provider} from 'react-redux'
+import {store} from './redux/store'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+>>>>>>> 20fe7f9 (To do list app task add, delete, mark as complete (complete))
   </StrictMode>,
 )
